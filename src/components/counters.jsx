ae15760fc09e2fb6286dfd9c86a0bdb6e9b3a236
Obinna Anyim <obinnanyim@gmail.com>
2020-12-04 03:46:14 +0100

    Component: Updating the State

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,15 +11,17 @@ class Counters extends Component {
         ]
     };
     
-    handleDelete = () => {
-        console.log('Delete Event handled....');
+    handleDelete = counterId => {
+        console.log('Delete Event handled....', counterId);
+        const counters = this.state.counters.filter(c => c.id !== counterId);
+        this.setState({ counters });
     }
     
     render() {
         return (
             <div>
                 {this.state.counters.map(counter => (
-                    <Counter key={ counter.id } onDelete={ this.handleDelete } value={ counter.value } />
+                    <Counter key={ counter.id } onDelete={ this.handleDelete } counter={counter} />
                 ))}
             </div>
         );
