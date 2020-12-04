b30cb6cbbae025e3e3e980e996a050b7ebbb72a2
Obinna Anyim <obinnanyim@gmail.com>
2020-12-04 15:47:41 +0100

    Component: Removing the Local State

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,38 +1,45 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = {
-        count: this.props.counter.value,
-        //tags: ["tag1","tag2","tag3"]
-    };
+    // state = {
+    //     count: this.props.counter.value,
+    //     //tags: ["tag1","tag2","tag3"]
+    // }; // This is a controlled component with no state. 
+    // It handles raising of events for the parent component.
+    
     // styles = {
     //     fontSize: 10,
     //     fontWeight: 'bold'
     // };
+    
     // constructor(){
     //     super();
     //     //console.log('Constructor', this);
     //     this.handleIncrement = this.handleIncrement.bind(this); // Binding event handlers
     // }
-    handleIncrement = () => {
-       // console.log("Increment Clicked.....", this);
-       this.setState({count: this.state.count + 1});
-       //console.log(prod);
-    }
+    
+    // handleIncrement = () => {
+    //    // console.log("Increment Clicked.....", this);
+    //    this.setState({count: this.state.count + 1});
+    //    //console.log(prod);
+    // } This should be done in the parent component.
+    
     // doHandleIncrement = ()=> {
     //     this.handleIncrement({id:1});
     // }
-    renderTags(){
-        if(this.state.tags.length === 0) return <p>There are no tags!</p>;
-        return <ul> { this.state.tags.map(tag => <li key={tag}> {tag} </li>) } </ul>
-    }
+    
+    // renderTags(){
+    //     if(this.state.tags.length === 0) return <p>There are no tags!</p>;
+    //     return <ul> { this.state.tags.map(tag => <li key={tag}> {tag} </li>) } </ul>
+    // }
+    
     render() {
         //console.log('props: ', this.props);
         return( 
         <div>
             {/* { this.props.children} */}
             <span className = { this.getBadgeClasses() } > { this.formatCount() } </span>
-            <button onClick={ this.handleIncrement} className = "btn btn-secondary btn-sm"> Increment </button>
+            <button onClick={() => this.props.onIncrement(this.props.counter)} className = "btn btn-secondary btn-sm"> Increment </button>
             <button onClick={() => this.props.onDelete(this.props.counter.id) } className="btn btn-danger btn-sm m-2">Delete</button>
             {/* { this.state.tags.length === 0 && 'Please create a new tag....'}
             { this.renderTags()} */}
@@ -41,13 +48,13 @@ class Counter extends Component {
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.props.counter.value === 0 ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-        const { count } = this.state;
-        return count === 0 ? "Zero" : count;
+        const { value } = this.props.counter;
+        return value === 0 ? "Zero" : value;
     }
 }
 
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -19,6 +19,17 @@ class Counters extends Component {
         this.setState({ counters });
     }
     
+    handleIncrement = counter => {
+        //console.log(counter);
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        //console.log(counters[index]);
+        counters[index] = {...counter};
+        counters[index].value++;
+        // console.log(this.state.counters[index]);
+        this.setState({ counters });
+    }
+    
     handleDelete = counterId => {
         console.log('Delete Event handled....', counterId);
         const counters = this.state.counters.filter(c => c.id !== counterId);
@@ -31,7 +42,12 @@ class Counters extends Component {
                 <button onClick={ this.handleReset }
                 className="btn btn-primary btn-sm m-2">Reset</button>
                 {this.state.counters.map(counter => (
-                    <Counter key={ counter.id } onDelete={ this.handleDelete } counter={counter} />
+                    <Counter 
+                        key={ counter.id } 
+                        onDelete={ this.handleDelete }
+                        onIncrement={ this.handleIncrement } 
+                        counter={counter} 
+                    />
                 ))}
             </div>
         );
